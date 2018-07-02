fee7b0bfc7bda59290ba4c311356618843cb5e4a
GainsGroup <38703232+GainsGroup@users.noreply.github.com>
2018-07-02 15:01:49 -0700

    Update plotting.R

--- base-report/report/plotting.R
+++ base-report/report/plotting.R
@@ -685,7 +685,7 @@ dot_plot <-  function(overall, type = 'lateral',title='Graph') {
                  size=0.1) +   # Draw dashed lines
     geom_hline(yintercept=50, size = .5) +
     labs(title=title,
-         subtitle="Percent Rank (NBA Players)") +
+         subtitle="Percent Rank(NBA)") +
     scale_colour_manual(values=c("FALSE"=dkred,"TRUE"=dkgrey)) +
     theme_p3() +
     coord_flip() +
