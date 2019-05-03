1f80095ad4327e573c526840322286304a258ab5
GainsGroup <38703232+GainsGroup@users.noreply.github.com>
2019-05-02 21:47:43 -0700

    Update v2_pdf_layout.R

--- base-report/report_v2/v2_pdf_layout.R
+++ base-report/report_v2/v2_pdf_layout.R
@@ -113,17 +113,17 @@ print(get_logo(), vp = vplayout(2, 21:23))
 ## ROW 3/4: summary text and scores
 print(drawtable_pageone(bio_stats_df), vp = vplayout(3:6, 6:20))
 #print(drawtable(performance_stats_df), vp = vplayout(3:9, 13:24)) #this is directionally right, placement wise 
-print(drawscore(athletecism_score, 'Athleticism', dkgrey), vp = vplayout(9:13, 15:19)) #move this guy down??
-print(drawscore(mechanics_score, 'Mechanics', dkred), vp = vplayout(9:13, 20:24)) #move this guy down ??
+print(drawscore(athletecism_score, 'Athleticism', dkgrey), vp = vplayout(8:12, 15:19)) #move this guy down??
+print(drawscore(mechanics_score, 'Mechanics', dkred), vp = vplayout(8:12, 20:24)) #move this guy down ??
 
 ## ROW 5-6: lollipop plot and figure
-print(main_page_1_dot_plot(), vp = vplayout(8:14, 2:12))  #pull from right df, move to right area
+print(main_page_1_dot_plot(), vp = vplayout(7:13, 2:12))  #pull from right df, move to right area
 
 ### The V man
 print(fig_one, vp = vplayout(14:27, 13:25))  ### move mroe bottom right 
 
 ## ROW 7-8: 2x2 scatter
-print(cluster_scatter, vp = vplayout(16:27, 2:12)) 
+print(cluster_scatter, vp = vplayout(15:26, 2:12)) 
 
 
 
