2046360d29c553500f8a6f10abc32f88715b126d
GainsGroup <38703232+GainsGroup@users.noreply.github.com>
2019-05-03 12:11:40 -0700

    Update v2_pdf_layout.R

--- base-report/report_v2/v2_pdf_layout.R
+++ base-report/report_v2/v2_pdf_layout.R
@@ -158,7 +158,7 @@ print(drawtable(skater %>% dplyr::rename("Lateral: Skater"=metric), fill_col = '
 
 ## ROW 3-5: Accel/Decel plot
 print(accel_plot2, vp = vplayout(3:11, 12:25))
-print(drawtext(page_2_detail, header = FALSE), vp = vplayout(12:15, 12:25))
+print(drawtext(page_2_detail, "Archetype Analysis", header = FALSE), vp = vplayout(12:15, 12:25))
 
 ## ROW 7-8: radar and cluster plots
 #print(radar_plot_athl, vp = vplayout(16:27, 12:26)) 
