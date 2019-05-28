cdfebda94201bddfffdebb0d96c673815fc0002d
GainsGroup <38703232+GainsGroup@users.noreply.github.com>
2019-05-28 13:02:08 -0700

    Update v2_draw_objects.R

--- base-report/report_v2/v2_draw_objects.R
+++ base-report/report_v2/v2_draw_objects.R
@@ -115,7 +115,7 @@ drawtable <- function(df, fill_col = NULL, fill = dkgrey, width=NULL) {
 drawtable_pagethree <- function(df, fill_col = NULL, fill = dkgrey, width=NULL,area, title) {
   header <- as.character(title)
   
-  df <- df %>% filter(type == area) %>% select(label, percentile) %>%rename(header = label, "Percentile" = percentile)
+  df <- df %>% filter(type == area) %>% select(label, percentile) %>% rename("Percentile" = percentile)
   
   g2 <- tableGrob(df, rows = NULL, theme = p3_table_theme())
   if (!is.null(fill_col)){
