8b5cbd8ae94494d36cb2d13a13174c96c7d1f193
GainsGroup <38703232+GainsGroup@users.noreply.github.com>
2019-05-28 20:05:53 -0700

    Update v2_draw_objects.R

--- base-report/report_v2/v2_draw_objects.R
+++ base-report/report_v2/v2_draw_objects.R
@@ -256,7 +256,7 @@ drawtext_p2 <- function(s, plot_title = NULL) {
     annotation_custom(
       grob = grob1,
       xmin = 0,
-      xmax = .95,
+      xmax = .985,
       ymin = .1,
       ymax = .9
     ) +
