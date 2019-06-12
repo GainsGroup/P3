b21ba7148aa563204e73538ac48909ea2cf82b9c
GainsGroup <38703232+GainsGroup@users.noreply.github.com>
2019-06-12 10:28:52 -0700

    Update v2_pdf_layout.R

--- base-report/magic_report/v2_pdf_layout.R
+++ base-report/magic_report/v2_pdf_layout.R
@@ -153,11 +153,11 @@ print(get_logo(), vp = vplayout(2, 23:25))
 
 ## ROW 3-6: Table
 drop_jump <- percentiles_page2 %>% filter(test_type=="Drop Jump") %>% select(metric, Percentile)
-print(drawtable(drop_jump %>% dplyr::rename("Vertical: Drop Jump"=metric), fill_col = 'Percentile', fill = dkgrey, width='fill'), vp = vplayout(3:10, 2:11), newpage=FALSE)
+print(drawtable(drop_jump %>% dplyr::rename("Vertical: Drop Jump"=metric), fill_col = 'Pos. Percentile', fill = dkgrey, width='fill'), vp = vplayout(3:10, 2:11), newpage=FALSE)
 st_vert <- percentiles_page2 %>% filter(test_type=="Standing Vertical") %>% select(metric, Percentile)
-print(drawtable(st_vert %>% dplyr::rename("Vertical: Standing"=metric), fill_col = 'Percentile', fill = dkgrey, width='fill'), vp = vplayout(11:18, 2:11), newpage=FALSE)
+print(drawtable(st_vert %>% dplyr::rename("Vertical: Standing"=metric), fill_col = 'Pos. Percentile', fill = dkgrey, width='fill'), vp = vplayout(11:18, 2:11), newpage=FALSE)
 skater <- percentiles_page2 %>% filter(test_type=="1 Off Skater") %>% select(metric, Percentile)
-print(drawtable(skater %>% dplyr::rename("Lateral: Skater"=metric), fill_col = 'Percentile', fill = dkgrey, width='fill'), vp = vplayout(19:26, 2:11), newpage=FALSE)
+print(drawtable(skater %>% dplyr::rename("Lateral: Skater"=metric), fill_col = 'Pos. Percentile', fill = dkgrey, width='fill'), vp = vplayout(19:26, 2:11), newpage=FALSE)
 
 print(drawtext_p2("*Cell-shade darkens as the percent-rank increases"), vp = vplayout(27:28, 1:15))
 
