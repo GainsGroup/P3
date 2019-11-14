63ca51d77000e26d373a66449af2e948f8c6ca55
GainsGroup <38703232+GainsGroup@users.noreply.github.com>
2019-11-14 09:52:50 -0800

    Update baseball_pdf_layout.R

--- base-report/baseball-report/baseball_pdf_layout.R
+++ base-report/baseball-report/baseball_pdf_layout.R
@@ -116,114 +116,36 @@ pdf(
 ###########
 newpage(grid)
 
-## Page header: date
+# 1 
+## Page header: date and title
 print(drawtext(paste0('Assessment Date: ', date)), vp = vplayout(1, 3:25))
-
-## ROW 2: page title
+## page title
 print(drawtext(intro_page_one, pagetitle, header = TRUE), vp = vplayout(2, 3:20))
 print(get_logo(), vp = vplayout(2, 21:23))
 
+# 2 Bio info table 
 ## ROW 3/4: summary text and scores
 print(drawtable_pageone(bio_stats_df), vp = vplayout(3:6, 6:20))
-#print(drawtable(performance_stats_df), vp = vplayout(3:9, 13:24)) #this is directionally right, placement wise 
-print(drawscore(athletecism_score, 'Athleticism', dkgrey), vp = vplayout(8:12, 15:19)) #move this guy down??
-print(drawscore(mechanics_score, 'Mechanics', dkred), vp = vplayout(8:12, 20:24)) #move this guy down ??
 
-## ROW 5-6: lollipop plot and figure
-print(main_page_1_dot_plot(), vp = vplayout(7:13, 2:12))  #pull from right df, move to right area
-
-### The V man
-print(fig_one, vp = vplayout(14:27, 13:25))  ### move mroe bottom right 
-
+# 3 Vert-Lat 2x2
 ## ROW 7-8: 2x2 scatter
-print(cluster_scatter, vp = vplayout(15:26, 2:12)) 
+print(cluster_scatter, vp = vplayout(15:26, 2:12)) ### Make this top right (currently bottom left)
 
+# 4 Spider Plot 
+#print(radar_plot_athl, vp = vplayout(16:27, 12:26))  ## Make bottom left 
 
-
-###########
-## PAGE 2##
-###########
-newpage(grid)
-
-## Page header: date
-print(drawtext(paste0('Assessment Date: ', date)), vp = vplayout(1, 2:25))
-## ROW 2: page title
-print(drawtext(intro_page_two, 'P3 ATHLETICISM SUMMARY', header = TRUE), vp = vplayout(2, 2:16))
-print(drawscore(athletecism_score, 'Athleticism', dkgrey), vp = vplayout(2, 18:22))
-print(get_logo(), vp = vplayout(2, 23:25))
-
-## ROW 3-6: Table
+# 5 Table 
 drop_jump <- percentiles_page2 %>% filter(test_type=="Drop Jump") %>% select(metric, Percentile)
 print(drawtable(drop_jump %>% dplyr::rename("Vertical: Drop Jump"=metric), fill_col = 'Percentile', fill = dkgrey, width='fill'), vp = vplayout(3:10, 2:11), newpage=FALSE)
 st_vert <- percentiles_page2 %>% filter(test_type=="Standing Vertical") %>% select(metric, Percentile)
 print(drawtable(st_vert %>% dplyr::rename("Vertical: Standing"=metric), fill_col = 'Percentile', fill = dkgrey, width='fill'), vp = vplayout(11:18, 2:11), newpage=FALSE)
 skater <- percentiles_page2 %>% filter(test_type=="1 Off Skater") %>% select(metric, Percentile)
-print(drawtable(skater %>% dplyr::rename("Lateral: Skater"=metric), fill_col = 'Percentile', fill = dkgrey, width='fill'), vp = vplayout(19:26, 2:11), newpage=FALSE)
 
-print(drawtext_p2("*Cell-shade darkens as the percent-rank increases"), vp = vplayout(27:28, 1:15))
-
-
-## ROW 3-5: Accel/Decel plot
-print(accel_plot2, vp = vplayout(3:11, 12:25))
-print(drawtext_p2(page_2_detail, "Archetype Analysis"), vp = vplayout(12:15, 12:26))
-
-## ROW 7-8: radar and cluster plots
-#print(radar_plot_athl, vp = vplayout(16:27, 12:26)) 
-print(clusterplot(athlete_label = athlete_prediction), vp = vplayout(16:26, 12:25))
-
-###########
-## PAGE 3##
-###########
-newpage(grid)
-
-## Page header: date
-print(drawtext(paste0('Assessment Date: ', date)), vp = vplayout(1, 2:25))
-
-## ROW 2: page title
-print(drawtext(intro_page_three, 'P3 MECHANICS SUMMARY', header = TRUE), vp = vplayout(2, 2:16))
-print(drawscore(mechanics_score, 'Mechanics', dkred), vp = vplayout(2, 18:22))
-print(get_logo(), vp = vplayout(2, 23:25))
-
-## ROW 3-6 LEFT
-
-## Man Figure
-print(fig_three, vp = vplayout(4:20, 10:26))
-
-
-## Low Back
-print(dot_plot1, vp = vplayout(3:7, 2:10))
-#print(drawtable_pagethree(percentiles_page3, fill_col = 'Percentile', fill = dkgrey, width = 'fill', area = 'low back', title = "Low Back"), 
-#      vp = vplayout(3:7, 2:10))
-
-## Left Knee
-print(dot_plot2, vp = vplayout(8:12, 2:10))
-#print(drawtable_pagethree(percentiles_page3, fill_col = 'Percentile', fill = dkgrey, width = 'fill', area = 'left knee', title = "Low Back"), 
-#      vp = vplayout(8:12, 2:10))
-## Left Foot
-print(dot_plot4, vp = vplayout(18:22, 2:10))
-#print(drawtable_pagethree(percentiles_page3 , fill_col = 'Percentile', fill = dkgrey, width = 'fill', area = 'left foot', title = "Low Back"), 
-#      vp = vplayout(13:17, 2:10))
-## Right Knee
-print(dot_plot3, vp = vplayout(13:17, 2:10))
-#print(drawtable_pagethree(percentiles_page3 , fill_col = 'Percentile', fill = dkgrey, width = 'fill', area = 'right knee', title = "Low Back"), 
-#      vp = vplayout(18:22, 2:10))
-## Right Foot
-print(dot_plot5, vp = vplayout(23:27, 2:10))
-#print(drawtable_pagethree(percentiles_page3 , fill_col = 'Percentile', fill = dkgrey, width = 'fill', area = 'right foot', title = "Low Back"), 
- #     vp = vplayout(23:27, 2:10))
-
-## ROW 7-8 LEFT: training targets and cluster plots
+# 6 Training Targets 
 print(drawtext(paste0(rec_one), 'Training Targets', header = FALSE), vp = vplayout(21:23, 12:24))
 print(drawtext(paste0(rec_two), title = NULL, header = FALSE), vp = vplayout(24:25, 12:24))
 print(drawtext(paste0(rec_three), title = NULL, header = FALSE), vp = vplayout(26:27, 12:24))
 
-##############
-## GLOSSARY###
-##############
-
-newpage(grid)
-
-print(get_glossary(), vp=vplayout(1:26,1:26))
 
 dev.off()
 
